7875039d807e8dd80cfd1145bb6c9f51c49b445e
long.phan <long.phan@netpower.no>
2023-06-09 23:53:12 +0700

    CylinderGeometry

--- threejs-tour/src/App.vue
+++ threejs-tour/src/App.vue
@@ -21,18 +21,18 @@ export default {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const geometry = new THREE.CylinderGeometry(1, 1, 3, 32);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    const cylinder = new THREE.Mesh(geometry, material);
+    scene.add(cylinder);
 
     camera.position.z = 5;
 
     function animate() {
       requestAnimationFrame(animate);
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      cylinder.rotation.x += 0.01;
+      cylinder.rotation.y += 0.01;
 
       renderer.render(scene, camera);
     }
